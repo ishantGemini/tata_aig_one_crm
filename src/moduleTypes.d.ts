a1be4452fee0e666c51211dcce01da45b6d5678c
Ishant Sahni <ishant.sahni@geminisolutions.com>
2024-01-04 16:24:30 +0530

    sso added

--- src/moduleTypes.d.ts
+++ src/moduleTypes.d.ts
@@ -1,6 +1,22 @@
 /* eslint-disable no-undef */
-declare module "Shared/Header" {
-  const CounterAppOne: React.ComponentType;
 
-  export default CounterAppOne;
+declare module "Shared/Header" {
+  const Header: React.ComponentType;
+
+  export default Header;
+}
+
+declare module "Shared/CustomSharedButton" {
+  interface Props {
+    text: string;
+    className?: string;
+    ButtonIcon?: IconType;
+    isLoading?: boolean;
+    onClick?: any;
+    classToOverride?: string;
+    type?: any;
+  }
+  const CustomSharedButton: React.ComponentType<Props>;
+
+  export default CustomSharedButton;
 }
